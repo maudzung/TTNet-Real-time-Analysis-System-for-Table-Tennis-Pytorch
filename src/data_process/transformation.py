fix: Optimize the normalization performance

src/data_process/transformation.py
@@ -17,19 +17,16 @@ class Compose(object):
 
 
 class Normalize():
-    def __init__(self, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), p=1.0):
+    def __init__(self, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), num_frames_sequence=9, p=1.0):
         self.p = p
-        self.mean = np.array(mean).reshape(1, 1, 3)
-        self.std = np.array(std).reshape(1, 1, 3)
+        self.mean = np.repeat(np.array(mean).reshape(1, 3, 1, 1), repeats=num_frames_sequence, axis=1)
+        self.std = np.repeat(np.array(std).reshape(1, 3, 1, 1), repeats=num_frames_sequence, axis=1)
 
     def __call__(self, imgs, ball_position_xy, seg_img):
         if random.random() < self.p:
             h, w, c = imgs.shape
             assert ((h == 128.) and (w == 320.) and (c == 27)), "The image need to be resized first"
-            num_imgs = int(c / 3)
-            for i in range(num_imgs):
-                img = imgs[:, :, (3 * i): (3 * (i + 1))]
-                imgs[:, :, (3 * i): (3 * (i + 1))] = ((img / 255.) - self.mean) / self.std
+            imgs = ((imgs / 255.) - self.mean) / self.std
 
         return imgs, ball_position_xy, seg_img
 