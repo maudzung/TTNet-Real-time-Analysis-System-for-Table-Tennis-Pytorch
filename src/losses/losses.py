842813417a65e924aebec48233fdb2b746107500
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2020-05-18 14:10:53 +0900

    feat: Add Multi_Task_Learning_Model

--- src/losses/losses.py
+++ src/losses/losses.py
@@ -71,10 +71,12 @@ class Segmentation_Loss(nn.Module):
 
 
 class Compute_Loss(nn.Module):
-    def __init__(self, num_events=2, weights_events=(1, 3), thresh_seg=0.5, reduction='mean'):
+    def __init__(self, num_events=2, weights_events=(1, 3), thresh_seg=0.5, input_size=(320, 128), reduction='mean'):
         super(Compute_Loss, self).__init__()
         self.num_events = num_events
-        self.ball_loss_criterion = Ball_Detection_Loss(w=320., h=128., reduction=reduction)
+        self.w = input_size[0]
+        self.h = input_size[1]
+        self.ball_loss_criterion = Ball_Detection_Loss(self.w, self.h, reduction=reduction)
         self.event_loss_criterion = Events_Spotting_Loss(weights=weights_events, num_events=num_events,
                                                          reduction=reduction)
         self.seg_loss_criterion = Segmentation_Loss(thresh_seg=thresh_seg, reduction='mean')
@@ -84,4 +86,38 @@ class Compute_Loss(nn.Module):
         event_loss = self.event_loss_criterion(pred_events, target_events)
         seg_loss = self.seg_loss_criterion(pred_seg, target_seg)
         total_loss = ball_loss + event_loss + seg_loss
+
         return total_loss
+
+
+class Multi_Task_Learning_Model(nn.Module):
+    """
+    Original paper: "Multi-task learning using uncertainty to weigh losses for scene geometry and semantics" - CVPR 2018
+    url: https://arxiv.org/pdf/1705.07115.pdf
+    """
+
+    def __init__(self, model, num_tasks, num_events=2, weights_events=(1, 3), thresh_seg=0.5, input_size=(320, 128),
+                 reduction='mean'):
+        super(Multi_Task_Learning_Model, self).__init__()
+        self.model = model
+        self.num_tasks = num_tasks
+        self.vars = nn.Parameter(torch.zeros((num_tasks)))
+        self.w = input_size[0]
+        self.h = input_size[1]
+        self.ball_loss_criterion = Ball_Detection_Loss(self.w, self.h, reduction=reduction)
+        self.event_loss_criterion = Events_Spotting_Loss(weights=weights_events, num_events=num_events,
+                                                         reduction=reduction)
+        self.seg_loss_criterion = Segmentation_Loss(thresh_seg=thresh_seg, reduction='mean')
+
+    def forward(self, original_batch_input, resize_batch_input, target_ball_position, target_events, target_seg):
+        pred_ball_position_global, pred_ball_position_local, pred_events, pred_seg = self.model(original_batch_input,
+                                                                                                resize_batch_input)
+        global_ball_loss = self.ball_loss_criterion(pred_ball_position_global, target_ball_position)
+        local_ball_loss = self.ball_loss_criterion(pred_ball_position_local, target_ball_position)
+        event_loss = self.event_loss_criterion(pred_events, target_events)
+        seg_loss = self.seg_loss_criterion(pred_seg, target_seg)
+        loss = 0.
+        for subloss_idx, subloss in enumerate([global_ball_loss, local_ball_loss, event_loss, seg_loss]):
+            loss += subloss / (self.vars[subloss_idx] ** 2) + torch.log(self.vars[subloss_idx])
+
+        return loss, self.vars.data.tolist()
