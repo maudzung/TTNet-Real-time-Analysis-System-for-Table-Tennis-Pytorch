82978d0566522c0bf9c9fe03be1b0e0dd6688b0a
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2020-05-18 15:29:36 +0900

    feat: training and validation phase code

---
+++ src/training/main.py
@@ -0,0 +1,179 @@
+import argparse
+import time
+import numpy as np
+import sys
+import random
+import os
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from torchsummary import summary
+
+
+sys.path.append('../')
+
+from data_process.ttnet_dataloader import create_train_val_dataloader, create_test_dataloader
+from training.train_utils import get_model, get_optimizer, get_lr_scheduler, get_saved_state, get_metrics, \
+    write_sumup_results
+from utils.misc import AverageMeter, save_checkpoint
+from utils.logger import create_logger
+from config.config import parse_configs
+
+
+def train_one_epoch(train_loader, model, optimizer, epoch, configs, logger):
+    time_infor = AverageMeter()
+    losses = AverageMeter()
+
+    # switch to train mode
+    model.train()
+    start_time = time.time()
+    for b_idx, (origin_imgs, aug_imgs, target_ball_position, target_events_spotting, seg_img, _, _) in enumerate(
+            train_loader):
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        b_size = origin_imgs.size(0)
+        target_ball_position = target_ball_position.to(configs.device)
+        target_events_spotting = target_events_spotting.to(configs.device)
+        target_seg = seg_img.to(configs.device)
+
+        aug_imgs = aug_imgs.to(configs.device).float()
+        # origin_imgs = origin_imgs.to(configs.device).float()
+
+        # compute output
+        with torch.set_grad_enabled(True):
+            pred_ball_position_global, pred_ball_position_local, pred_events, pred_seg, total_loss, _ = model(
+                origin_imgs, aug_imgs, target_ball_position, target_events_spotting, target_seg)
+
+            # compute gradient and perform backpropagation
+            total_loss.backward()
+            optimizer.step()
+
+        losses.update(total_loss.item(), b_size)
+        # measure elapsed time
+        time_infor.update(time.time() - start_time)
+
+        if ((b_idx + 1) % configs.print_freq) == 0:
+            print_string = '\t--- Epoch [{}/{}] Iter [{}/{}]\t'.format(epoch, configs.train_num_epochs, b_idx,
+                                                                       len(train_loader))
+            print_string += 'Loss value: {:.5f}, avg: {:.5f}\t'.format(losses.val, losses.avg)
+            print_string += 'time: {:.2f}s\t'.format(time_infor.val)
+            logger.info(print_string)
+
+    return losses.avg
+
+
+def validate_one_epoch(val_loader, model, epoch, configs, logger):
+    time_infor = AverageMeter()
+    losses = AverageMeter()
+    # switch to evaluate mode
+    model.eval()
+    start_time = time.time()
+    for b_idx, (origin_imgs, aug_imgs, target_ball_position, target_events_spotting, seg_img, _, _) in enumerate(
+            val_loader):
+        b_size = origin_imgs.size(0)
+        target_ball_position = target_ball_position.to(configs.device)
+        target_events_spotting = target_events_spotting.to(configs.device)
+        target_seg = seg_img.to(configs.device)
+
+        aug_imgs = aug_imgs.to(configs.device).float()
+        origin_imgs = origin_imgs.to(configs.device).float()
+        # compute output
+        with torch.set_grad_enabled(False):
+            pred_ball_position_global, pred_ball_position_local, pred_events, pred_seg, total_loss, _ = model(
+                origin_imgs, aug_imgs, target_ball_position, target_events_spotting, target_seg)
+
+        losses.update(total_loss.item(), b_size)
+        time_infor.update(time.time() - start_time)
+
+        # measure elapsed time
+        if ((b_idx + 1) % configs.print_freq) == 0:
+            print_string = '\t--- Epoch [{}/{}] Iter [{}/{}]\t'.format(epoch, configs.train_num_epochs, b_idx,
+                                                                       len(val_loader))
+            print_string += 'Loss value: {:.5f}, avg: {:.5f}\t'.format(losses.val, losses.avg)
+            print_string += 'time: {:.2f}s\t'.format(time_infor.val)
+            logger.info(print_string)
+
+    return losses.avg
+
+
+def main():
+    configs = parse_configs()
+
+    # Re-produce results
+    random.seed(configs.seed)
+    np.random.seed(configs.seed)
+    torch.manual_seed(configs.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    logger = create_logger(configs.logs_dir, configs.saved_fn)
+    logger.info('>>> Created a new logger')
+    logger.info('>>> configs: {}'.format(configs))
+
+    writer = SummaryWriter(log_dir=os.path.join(configs.logs_dir, 'tensorboard'))
+
+    logger.info(">>> Loading dataset & getting dataloader...")
+    # Create dataloader
+    train_loader, val_loader = create_train_val_dataloader(configs)
+    print('train_loader: {}, val_loader: {}'.format(len(train_loader), len(val_loader)))
+    # model
+    model = get_model(configs).to(configs.device)
+    # summary(model.cuda(), (27, 1024))
+    # Data Parallel
+    if configs.num_gpus > 1:
+        model = torch.nn.DataParallel(model)
+
+    optimizer = get_optimizer(configs, model, is_warm_up=False)
+    lr_scheduler = get_lr_scheduler(optimizer, configs)
+    best_val_loss = np.inf
+    lr = configs.train_lr
+    earlystop_count = 0
+    for epoch in range(1, configs.train_num_epochs + 1):
+        # train_loader, val_loader = get_dataloader(configs)
+        logger.info('{}'.format('*-' * 40))
+        logger.info('{} {}/{} {}'.format('=' * 35, epoch, configs.train_num_epochs, '=' * 35))
+        logger.info('{}'.format('*-' * 40))
+
+        logger.info('>>> Epoch: [{}/{}] learning rate: {}'.format(epoch, configs.train_num_epochs, lr))
+        # train for one epoch
+        train_loss = train_one_epoch(train_loader, model, optimizer, epoch, configs, logger)
+        # evaluate on validation set
+        val_loss = validate_one_epoch(val_loader, model, epoch, configs, logger)
+
+        is_best = val_loss <= best_val_loss
+        best_val_loss = min(val_loss, best_val_loss)
+        print_string = '\t--- train_loss: {:.4f}, val_loss: {:.4f}, best_val_loss: {:.4f}\t'.format(
+            train_loss,
+            val_loss,
+            best_val_loss)
+
+        writer.add_scalars('Loss', {'train': train_loss, 'val': val_loss}, epoch)
+
+        saved_state = get_saved_state(model, optimizer, epoch, configs)
+        save_checkpoint(configs.checkpoints_dir, configs.saved_fn, saved_state, is_best=is_best, logger=None)
+
+        # Adjust learning rate
+        lr_scheduler.step()
+        # Get next learning rate
+        for param_group in optimizer.param_groups:
+            lr = param_group['lr']
+
+        if configs.train_earlystop_patience:
+            earlystop_count = 0 if is_best else (earlystop_count + 1)
+            print_string += ' |||\t earlystop_count: {}'.format(earlystop_count)
+
+        logger.info(print_string)
+
+        if configs.train_earlystop_patience:
+            if configs.train_earlystop_patience <= earlystop_count:
+                logger.info('\t--- Early stopping!!!')
+                break
+            else:
+                logger.info('\t--- Continue training..., earlystop_count: {}'.format(earlystop_count))
+
+    writer.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/training/train_utils.py
@@ -0,0 +1,168 @@
+import torch
+import sys
+import copy
+import os
+
+from sklearn.metrics import f1_score, accuracy_score, confusion_matrix
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
+
+sys.path.append('../')
+
+from models.TTNet import TTNet
+from losses.losses import Imbalance_Loss_Model, Multi_Task_Learning_Model
+
+
+def get_model(configs):
+    """
+    Create model based on backbone name
+    Args:
+        configs:
+
+    Returns:
+
+    """
+    if configs.model_backbone == 'ttnet':
+        ttnet_model = TTNet(dropout_p=configs.model_dropout_p)
+    else:
+        assert False, 'Undefined model backbone'
+
+    if configs.multitask_learning == True:
+        model = Multi_Task_Learning_Model(ttnet_model, num_tasks=4, num_events=2, weights_events=(1, 3), thresh_seg=0.5,
+                                          input_size=(320, 128), reduction='mean')
+    else:
+        model = Imbalance_Loss_Model(ttnet_model, num_events=2, weights_events=(1, 3), thresh_seg=0.5,
+                                     input_size=(320, 128), reduction='mean')
+
+    return model
+
+
+def get_optimizer(configs, model, is_warm_up):
+    """
+    Initialize optimizer for training process
+    Args:
+        configs:
+        model:
+        is_warm_up:
+
+    Returns:
+
+    """
+    # trainable_vars = [param for param in model.module.parameters() if param.requires_grad]
+    if is_warm_up:
+        lr = configs.train_warmup_lr
+        momentum = configs.train_warmup_momentum
+        weight_decay = configs.train_warmup_weight_decay
+        optimizer_type = configs.train_warmup_optimizer_type
+    else:
+        lr = configs.train_lr
+        momentum = configs.train_momentum
+        weight_decay = configs.train_weight_decay
+        optimizer_type = configs.train_optimizer_type
+    if configs.num_gpus > 1:
+        train_params = model.module.parameters()
+    else:
+        train_params = model.parameters()
+
+    if optimizer_type == 'sgd':
+        optimizer = torch.optim.SGD(train_params, lr=lr, momentum=momentum, weight_decay=weight_decay)
+    elif optimizer_type == 'adam':
+        optimizer = torch.optim.Adam(train_params, lr=lr, weight_decay=weight_decay)
+    else:
+        assert False, "Unknown optimizer type"
+
+    return optimizer
+
+
+def get_lr_scheduler(optimizer, configs):
+    if configs.train_lr_type == 'step_lr':
+        lr_scheduler = StepLR(optimizer, step_size=configs.train_lr_step_size, gamma=configs.train_lr_factor)
+    elif configs.train_lr_type == 'plateau':
+        lr_scheduler = ReduceLROnPlateau(optimizer, factor=configs.train_lr_factor, patience=configs.train_lr_patience)
+    else:
+        raise TypeError
+
+    return lr_scheduler
+
+
+def get_saved_state(model, optimizer, epoch, configs):
+    """
+    Get the information to save with checkpoints
+    Args:
+        model:
+        optimizer:
+        epoch:
+        configs:
+
+    Returns:
+
+    """
+    saved_state = {
+        'epoch': epoch,
+        'configs': configs,
+        'optimizer': copy.deepcopy(optimizer.state_dict()),
+    }
+    if configs.num_gpus > 1:
+        saved_state['state_dict'] = copy.deepcopy(model.module.state_dict())
+    else:
+        saved_state['state_dict'] = copy.deepcopy(model.state_dict())
+
+    return saved_state
+
+
+def get_metrics(all_targets, all_preds):
+    """
+    Calculate evaluation metrics during training/validate/testing phase
+    Args:
+        all_targets:
+        all_preds:
+
+    Returns:
+
+    """
+    acc = accuracy_score(all_targets, all_preds) * 100.
+    micro_f1 = f1_score(all_targets, all_preds, average='micro')
+    macro_f1 = f1_score(all_targets, all_preds, average='macro')
+    confusion_mat = confusion_matrix(y_true=all_targets, y_pred=all_preds)
+
+    return acc, micro_f1, macro_f1, confusion_mat
+
+
+def write_sumup_results(configs, test_acc, test_micro_f1, test_macro_f1):
+    """
+    Write the results on test set to a file when the training process is done.
+    Args:
+        configs:
+        test_acc:
+        test_micro_f1:
+        test_macro_f1:
+
+    Returns:
+
+    """
+    result_filepath = os.path.join(configs.results_dir, 'sumup_results.csv')
+    if os.path.isfile(result_filepath):
+        f_write_results = open(result_filepath, 'a+')
+    else:
+        f_write_results = open(result_filepath, 'w')
+        row = 'model_name,num_points,min_num_points,'
+        row += 'loss_type,dropout_p,'
+        row += 'data_sampler,uniform,feature_transform,'
+        row += 'test_acc,test_micro_f1,test_macro_f1\n'
+        f_write_results.write(row)
+
+    row = '{},{},{},'.format(configs.model_backbone, configs.num_points, configs.min_num_points)
+    row += '{},{:.2f},'.format(configs.loss_type, configs.model_dropout_p)
+    row += 'Yes,' if configs.data_sampler else 'No,'
+    row += 'Yes,' if configs.uniform else 'No,'
+    row += 'Yes,' if configs.feature_transform else 'No,'
+    row += '{:.2f},{:.4f},{:.4f}\n'.format(test_acc, test_micro_f1, test_macro_f1)
+    f_write_results.write(row)
+
+    f_write_results.close()
+
+
+if __name__ == '__main__':
+    from config.config import get_default_configs
+
+    configs = get_default_configs()
+    model = get_model(configs)
