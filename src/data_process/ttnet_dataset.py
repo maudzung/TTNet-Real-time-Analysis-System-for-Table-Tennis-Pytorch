142b1be3335e786434742da170b2646251f30ba6
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2020-05-18 13:50:04 +0900

    feat: return both org and resize imgs

--- src/data_process/ttnet_dataset.py
+++ src/data_process/ttnet_dataset.py
@@ -10,9 +10,12 @@ from data_process.ttnet_data_utils import load_raw_img, create_target_ball_possi
 
 
 class TTNet_Dataset(Dataset):
-    def __init__(self, events_infor, events_dict, transformations=None):
+    def __init__(self, events_infor, events_dict, sigma=1., input_size=(320, 128), transformations=None):
         self.events_infor = events_infor
         self.events_dict = events_dict
+        self.sigma = sigma
+        self.w = input_size[0]
+        self.h = input_size[1]
         self.transformations = transformations
 
     def __len__(self):
@@ -21,29 +24,34 @@ class TTNet_Dataset(Dataset):
     def __getitem__(self, index):
         img_path_list, ball_position_xy, event_name, seg_path = self.events_infor[index]
         event_class = self.events_dict[event_name]
+        print('event_name: {}'.format(event_name))
         # Load segmentation
         seg_img = load_raw_img(seg_path)
 
         # Load list of images (-4, 4)
-        imgs = None
+        origin_imgs = None
         for img_path_idx, img_path in enumerate(img_path_list):
             img = load_raw_img(img_path)
             if img_path_idx == 0:
-                imgs = img
+                origin_imgs = img
             else:
-                imgs = np.concatenate((imgs, img), axis=-1)
+                origin_imgs = np.concatenate((origin_imgs, img), axis=-1)
 
         # Apply augmentation
-        if self.transformations:
-            imgs, ball_position_xy, seg_img = self.transformations(imgs, ball_position_xy, seg_img)
+        assert self.transformations is not None, "At lease, need to resize image to input_size"
+
+        aug_imgs, ball_position_xy, seg_img = self.transformations(origin_imgs, ball_position_xy, seg_img)
+        # Transpose (H, W, C) to (C, H, W) --> fit input of TTNet model
+        aug_imgs = aug_imgs.transpose(2, 0, 1)
 
         # Transpose (H, W, C) to (C, H, W) --> fit input of TTNet model
-        imgs = imgs.transpose(2, 0, 1)
+        origin_imgs = origin_imgs.transpose(2, 0, 1)
 
-        target_ball_possition = create_target_ball_possition(ball_position_xy, sigma=1., w=320., h=128.)
+        # Create target for events spotting and ball position
+        target_ball_possition = create_target_ball_possition(ball_position_xy, self.sigma, self.w, self.h)
         target_events_spotting = create_target_events_spotting(event_name, configs.events_dict)
 
-        return imgs, target_ball_possition, target_events_spotting, seg_img
+        return origin_imgs, aug_imgs, target_ball_possition, target_events_spotting, seg_img, ball_position_xy, event_name
 
 
 if __name__ == '__main__':
@@ -58,14 +66,26 @@ if __name__ == '__main__':
     dataset_type = 'training'
     train_events_infor, val_events_infor = train_val_data_separation(configs)
     print(len(train_events_infor))
-    ttnet_dataset = TTNet_Dataset(train_events_infor, configs.events_dict, transformations=None)
+    # Test transformation
+    transform = Compose([
+        Random_Crop(max_height_reduction_percent=0.15, max_width_reduction_percent=0.15, p=1.),
+        Resize(new_size=(320, 128), p=1.0),
+        Random_HFlip(p=1.),
+        Random_Rotate(rotation_angle_limit=15, p=1.)
+    ], p=1.)
+
+    ttnet_dataset = TTNet_Dataset(train_events_infor, configs.events_dict, transformations=transform)
 
     print(len(ttnet_dataset))
-    example_index = 150
-    imgs, event_name, ball_position_xy, seg_img = ttnet_dataset.__getitem__(example_index)
+    example_index = 300
+    origin_imgs, aug_imgs, target_ball_possition, target_events_spotting, seg_img, ball_position_xy, event_name = ttnet_dataset.__getitem__(
+        example_index)
+    print('target_ball_possition.shape: {}'.format(target_ball_possition.shape))
+    print('target_events_spotting.shape: {}'.format(target_events_spotting.shape))
+    print('seg_img: {}'.format(seg_img.shape))
 
-    imgs = imgs.transpose(1, 2, 0)
-    print(imgs.shape)
+    origin_imgs = origin_imgs.transpose(1, 2, 0)
+    print(origin_imgs.shape)
 
     out_images_dir = os.path.join(configs.working_dir, 'out_images')
     if not os.path.isdir(out_images_dir):
@@ -75,30 +95,25 @@ if __name__ == '__main__':
     axes = axes.ravel()
 
     for i in range(configs.num_frames_sequence):
-        img = imgs[:, :, (i * 3): (i + 1) * 3]
+        img = origin_imgs[:, :, (i * 3): (i + 1) * 3]
         axes[i].imshow(img)
         axes[i].set_title('image {}'.format(i))
     fig.suptitle(
         'Event: {}, ball_position_xy: (x= {}, y= {})'.format(event_name, ball_position_xy[0], ball_position_xy[1]),
         fontsize=16)
-    plt.savefig(os.path.join(out_images_dir, 'img_example_{}.jpg'.format(example_index)))
-    plt.imsave(os.path.join(out_images_dir, 'org_seg_img_example_{}.jpg'.format(example_index)), seg_img)
+    plt.savefig(os.path.join(out_images_dir, 'fig_img_{}.jpg'.format(example_index)))
+    plt.imsave(os.path.join(out_images_dir, 'seg_img_{}.jpg'.format(example_index)), seg_img)
 
-    # Test transformation
-    augment = Compose([
-        Random_Crop(max_height_reduction_percent=0.15, max_width_reduction_percent=0.15, p=1.),
-        Resize(new_size=(320, 128), p=1.0),
-        Random_HFlip(p=1.),
-        Random_Rotate(rotation_angle_limit=15, p=1.)
-    ], p=1.)
-    imgs, ball_position_xy, seg_img = augment(imgs, ball_position_xy, seg_img)
-    plt.imsave(os.path.join(out_images_dir, 'af_seg_img_crop_{}.jpg'.format(example_index)), seg_img)
+    aug_imgs = aug_imgs.transpose(1, 2, 0)
+    print('aug_imgs: {}'.format(aug_imgs.shape))
+
+    plt.imsave(os.path.join(out_images_dir, 'augment_seg_img_{}.jpg'.format(example_index)), seg_img)
     for i in range(configs.num_frames_sequence):
-        img = imgs[:, :, (i * 3): (i + 1) * 3]
+        img = aug_imgs[:, :, (i * 3): (i + 1) * 3]
         if (i == (configs.num_frames_sequence - 1)):
             ball_img = cv2.circle(img, tuple(ball_position_xy), radius=5, color=(255, 0, 0), thickness=2)
             ball_img = cv2.cvtColor(ball_img, cv2.COLOR_RGB2BGR)
-            cv2.imwrite(os.path.join(out_images_dir, 'af_crop_resize_ball_img_{}.jpg'.format(example_index)),
+            cv2.imwrite(os.path.join(out_images_dir, 'augment_img_{}.jpg'.format(example_index)),
                         ball_img)
 
         axes[i].imshow(img)
@@ -106,4 +121,4 @@ if __name__ == '__main__':
     fig.suptitle(
         'Event: {}, ball_position_xy: (x= {}, y= {})'.format(event_name, ball_position_xy[0], ball_position_xy[1]),
         fontsize=16)
-    plt.savefig(os.path.join(out_images_dir, 'af_crop_resize_img_{}.jpg'.format(example_index)))
+    plt.savefig(os.path.join(out_images_dir, 'augment_fig_img_{}.jpg'.format(example_index)))
