f0a9c7f5b4c75dc89a2edd2c1abcbdeeaa67b253
maudzung <nguyenmaudung93.kstn@gmail.com>
2020-05-25 00:55:07 +0900

    feat: Add progress data for logging information

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -34,6 +34,28 @@ class AverageMeter(object):
         return fmtstr.format(**self.__dict__)
 
 
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def get_message(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        return '\t'.join(entries)
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
 def save_checkpoint(checkpoints_dir, saved_fn, saved_state, is_best, logger=None):
     """
     Save checkpoint every epoch
