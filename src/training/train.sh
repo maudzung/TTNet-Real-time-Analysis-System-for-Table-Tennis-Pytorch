a9c89889f842fbd93990e48356b0b26807b19213
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2020-06-05 13:34:36 +0900

    feat: Add bash script for training

---
+++ src/training/train.sh
@@ -0,0 +1,17 @@
+python main.py \
+  --saved_fn 'ttnet_2020_06_05_official' \
+  --arch 'ttnet' \
+  --no-val \
+  --batch_size 32 \
+  --sigma 1. \
+  --thresh_ball_pos_mask 0.01 \
+  --start_epoch 1 \
+  --num_epochs 100 \
+  --lr 0.001 \
+  --lr_type 'step_lr' \
+  --lr_step_size 10 \
+  --lr_factor 0.5 \
+  --world-size 1 \
+  --rank 0 \
+  --dist-backend 'nccl' \
+  --multiprocessing-distributed
