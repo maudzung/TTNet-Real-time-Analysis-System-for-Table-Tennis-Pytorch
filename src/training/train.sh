a6b71e2bfd3e582b670ee7c9081df0f32fbbd3c7
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2020-06-07 11:47:21 +0900

    Feat: Add script for training 21 epochs without local and event module

--- src/training/train.sh
+++ src/training/train.sh
@@ -5,10 +5,11 @@ python main.py \
   --arch 'ttnet' \
   --no-val \
   --batch_size 32 \
+  --num_workers 8 \
   --sigma 1. \
   --thresh_ball_pos_mask 0.01 \
   --start_epoch 1 \
-  --num_epochs 100 \
+  --num_epochs 21 \
   --lr 0.001 \
   --lr_type 'step_lr' \
   --lr_step_size 10 \
@@ -18,7 +19,7 @@ python main.py \
   --dist-backend 'nccl' \
   --multiprocessing-distributed \
   --weight_decay 0. \
+  --global_weight 5. \
+  --seg_weight 1. \
   --no_local \
-  --no_event \
-  --global_weight 5.0 \
-  --seg_weight 1.0
+  --no_event
