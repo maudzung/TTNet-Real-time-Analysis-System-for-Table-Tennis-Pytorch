14e6b33a046c8c3c72adcf0c0f7fcbb3e8416ab9
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2020-06-07 14:13:05 +0900

    Feat: Add script for fine tune the TTNet
    - Train with 9 epochs
    - step_lr with step size = 5, initial lr is 1e-4

---
+++ src/training/train_full_fine_tune_all_modules.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+python main.py \
+  --saved_fn 'ttnet_full_freeze_global_freeze_seg' \
+  --arch 'ttnet' \
+  --no-val \
+  --batch_size 32 \
+  --num_workers 8 \
+  --sigma 1. \
+  --thresh_ball_pos_mask 0.01 \
+  --start_epoch 1 \
+  --num_epochs 9 \
+  --lr 0.0001 \
+  --lr_type 'step_lr' \
+  --lr_step_size 5 \
+  --lr_factor 0.2 \
+  --world-size 1 \
+  --rank 0 \
+  --dist-backend 'nccl' \
+  --multiprocessing-distributed \
+  --weight_decay 0. \
+  --global_weight 1. \
+  --seg_weight 1. \
+  --event_weight 2. \
+  --local_weight 1. \
+  --pretrained_path ../../checkpoints/ttnet_full_freeze_global_freeze_seg/ttnet_full_freeze_global_freeze_seg_epoch_21.pth
