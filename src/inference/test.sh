0346d9b1976c345dbbbdd4ccdec70dd0a9707627
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2020-06-07 14:36:58 +0900

    Feat: Add threshold for segmentation and event spotting

--- src/inference/test.sh
+++ src/inference/test.sh
@@ -4,4 +4,6 @@ python test.py \
   --gpu_idx 0 \
   --evaluate \
   --batch_size 1 \
-  --pretrained_path ../../checkpoints/ttnet_full_freeze_global_freeze_seg/ttnet_full_freeze_global_freeze_seg_epoch_21.pth
+  --pretrained_path ../../checkpoints/ttnet_full_freeze_global_freeze_seg/ttnet_full_freeze_global_freeze_seg_epoch_21.pth \
+  --seg_thresh 0.5 \
+  --event_thresh 0.5
