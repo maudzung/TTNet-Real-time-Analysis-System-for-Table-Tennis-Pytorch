8d317b87b4f972a4047715fef37320fba8fc7b95
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2020-06-10 18:39:26 +0900

    Feat: Add post-processing step

--- src/inference/demo.py
+++ src/inference/demo.py
@@ -14,12 +14,14 @@ import sys
 import torch
 
 import cv2
+import numpy as np
 
 sys.path.append('../')
 
 from data_process.ttnet_video_loader import TTNet_Video_Loader
 from training.train_utils import get_model, load_pretrained_model
 from config.config import parse_configs
+from inference.post_processing import post_processing
 
 
 def demo(configs):
@@ -51,27 +53,32 @@ def eval_seq(configs, video_loader, result_filename, show_image, frame_rate):
 
     assert configs.pretrained_path is not None, "Need to load the pre-trained model"
     model = load_pretrained_model(model, configs.pretrained_path, configs.gpu_idx, configs.overwrite_global_2_local)
-    # Load dataset
-    frame_idx = 0
-    for count, origin_imgs, resized_imgs in video_loader:
-        img = origin_imgs[0:3, :, :]  # Just dummy, change latter
-        resized_imgs = resized_imgs.to(configs.device, non_blocking=True).float()
-        origin_imgs = origin_imgs.to(configs.device, non_blocking=True).float()
-        pred_ball_global, pred_ball_local, pred_events, pred_seg, local_ball_pos_xy, total_loss, _ = model(
-            origin_imgs, resized_imgs, None, None, None, None)
-        img = img.transpose(1, 2, 0)
-        online_im = plot_detection(img)
 
-        if show_image:
-            cv2.imshow('online_im', online_im)
+    model.eval()
+    with torch.no_grad():
+        frame_idx = 0
+        for count, origin_imgs, resized_imgs in video_loader:
+            img = origin_imgs[0:3, :, :]  # Just dummy, change latter
+            resized_imgs = resized_imgs.to(configs.device, non_blocking=True).float().unsqueeze(0)
+            origin_imgs = origin_imgs.to(configs.device, non_blocking=True).float().unsqueeze(0)
+            pred_ball_global, pred_ball_local, pred_events, pred_seg = model.run_demo(origin_imgs, resized_imgs)
+            prediction_global, prediction_local, prediction_seg, prediction_events = post_processing(
+                pred_ball_global, pred_ball_local, pred_events, pred_seg, configs.input_size[0],
+                configs.thresh_ball_pos_prob, configs.seg_thresh, configs.event_thresh)
+            img = img.transpose(1, 2, 0)
+            ploted_img = plot_detection(img, prediction_global, prediction_local, prediction_seg, prediction_events)
 
-        if configs.save_demo_output:
-            cv2.imwrite(os.path.join(configs.saved_dir, '{:06d}.jpg'.format(frame_idx)), online_im)
+            if show_image:
+                cv2.imshow('ploted_img', ploted_img)
 
-        frame_idx += 1
+            if configs.save_demo_output:
+                cv2.imwrite(os.path.join(configs.saved_dir, '{:06d}.jpg'.format(frame_idx)), ploted_img)
+
+            frame_idx += 1
 
 
-def plot_detection(img):
+def plot_detection(img, prediction_global, prediction_local, prediction_seg, prediction_events):
+    """Show the predicted information in the image"""
     return img
 
 
